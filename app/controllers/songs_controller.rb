a9b9686cf10f811bf2dfa8f72b4cb2ba770e61b4
Olena Ageyeva <eageeva@hotmail.com>
2017-12-27 19:54:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song=Song.find_by_slug(params[:slug])
+    @message=""
     @message=@@message if @@message
     @@message=nil
     erb :'/songs/show'
