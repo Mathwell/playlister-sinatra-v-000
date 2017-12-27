18153618479a54838fc2e029e68c5799ff52b542
Olena Ageyeva <eageeva@hotmail.com>
2017-12-27 18:41:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song=Song.find_by_slug(params[:slug])
-    erb :edit
+    erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
